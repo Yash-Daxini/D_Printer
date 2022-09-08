abce8a03f45d9d1d61ed90a2807d00e618d4ce17
Infinity <200540107113@darshan.ac.in>
2022-09-09 00:56:26 +0530

    Routes changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,14 +4,14 @@ const Header = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-info fs-5">
                 <div className="container-fluid  d-flex justify-content-center">
-                    <Link className="navbar-brand fs-3 text-white fw-bolder" to="./">D Printers</Link>
+                    <Link className="navbar-brand fs-3 text-white fw-bolder" to="./D_Printer">D Printers</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active text-white fw-bold" aria-current="page" to="./">Home</Link>
+                                <Link className="nav-link active text-white fw-bold" aria-current="page" to="./D_Printer">Home</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link text-white fw-bold" to="./Printers">Printers</Link>
